b130a058d05b5eb77cf17cd52858a6dd3ece5d18
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-05-22 20:33:14 +0300

    добавила импорт

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+console.log(galleryItems);
+
+
+const gallaryContainer = document.querySelector('.gallery'); 
+
+const images = galleryItems.map(({preview, original, description}) => ` 
+ <li class="gallery__item"> 
+ <a class="gallery__link" href='${original}'> 
+//    <img class="gallery__image" src='${preview}' 
+     data-source ='${original}' 
+     alt='${description}' 
+   /> 
+ </a> 
+</li>`).join(''); 
+ 
+gallaryContainer.insertAdjacentHTML('beforeend', images); 
+
+gallaryContainer.addEventListener('click', onImageClick); 
+
+function onImageClick (evt){ 
+    
+    // запрет стандартных действий
+    evt.preventDefault(); 
+
+    // проверка на картинку
+    if(evt.target.nodeName !== "IMG") {
+      return;
+    }
+    
+    // а если клацаем на картинку
+    const instance = simpleLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height = "600">` 
+    ); 
+    instance.show(); 
+
+    // закрытие
+    gallaryContainer.addEventListener('keydown', (evt) => {
+      if(evt.code === 'Escape') {
+        instance.close()
+      }
+    })
+};
