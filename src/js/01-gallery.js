4caad3a7aaeb2940e4dce5072c50152ff83db4e8
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-05-22 20:40:12 +0300

    сделала первое задание

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,45 +7,22 @@ console.log(galleryItems);
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
-console.log(galleryItems);
-
-
 const gallaryContainer = document.querySelector('.gallery'); 
 
-const images = galleryItems.map(({preview, original, description}) => ` 
- <li class="gallery__item"> 
- <a class="gallery__link" href='${original}'> 
-//    <img class="gallery__image" src='${preview}' 
-     data-source ='${original}' 
-     alt='${description}' 
-   /> 
- </a> 
+const images = galleryItems.map(({preview, original, description}) => `
+
+<li class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src='${preview}' alt='${description}' />
+   </a>
 </li>`).join(''); 
  
 gallaryContainer.insertAdjacentHTML('beforeend', images); 
 
-gallaryContainer.addEventListener('click', onImageClick); 
 
-function onImageClick (evt){ 
-    
-    // запрет стандартных действий
-    evt.preventDefault(); 
+const gallaryLightboxContainer = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 
-    // проверка на картинку
-    if(evt.target.nodeName !== "IMG") {
-      return;
-    }
-    
-    // а если клацаем на картинку
-    const instance = simpleLightbox.create(`
-      <img src="${evt.target.dataset.source}" width="800" height = "600">` 
-    ); 
-    instance.show(); 
+   
 
-    // закрытие
-    gallaryContainer.addEventListener('keydown', (evt) => {
-      if(evt.code === 'Escape') {
-        instance.close()
-      }
-    })
-};
