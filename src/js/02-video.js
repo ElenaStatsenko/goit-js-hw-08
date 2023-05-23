f699947f57c70918e73fbffe3a88784aace53e29
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-05-23 21:45:50 +0300

    попыталась сделать 2 задачу

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,10 @@
-import throttle = require("lodash.throttle");
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+const iframe = document.querySelector("iframe");
+const player = new Vimeo.Player(iframe);
+const onPlay = function(data) {
+    
+};
+
+player.on('play', onPlay);
