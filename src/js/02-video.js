2f8aeb38123f01c80a4021516e74646a5d647090
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-05-25 22:06:59 +0300

    cсделала второе задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,9 +2,34 @@
 import Player from "@vimeo/player";
 import throttle from "lodash.throttle";
 const iframe = document.querySelector("iframe");
-const player = new Vimeo.Player(iframe);
-const onPlay = function(data) {
-    
-};
+const player = new Player(iframe);
+player.on('timeupdate', throttle (function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000)
+);
+const currentTime = localStorage.getItem("videoplayer-current-time");
+if (currentTime) {player.setCurrentTime(currentTime);
+}
 
-player.on('play', onPlay);
+
+// нашла в документации pre-exist
+// import Player from '@vimeo/player';
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+// плеер он const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+// добавление события
+// player.on('timeupdate', trottle (function (data) {
+    // function(data) {
+//     // data is an object containing properties specific to that event
+// });
