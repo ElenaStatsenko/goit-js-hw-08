a26ce25f6b66ffb592f87c7074281d4f3ca4def6
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-05-30 17:49:17 +0000

    Deploying to gh-pages from @ ElenaStatsenko/goit-js-hw-08@e17da55f7b0b354015aadd6d66f9bb07e3b19a7d 🚀

---
+++ 03-feedback.d444d1e2.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),email:document.querySelector(".feedback-form email"),textarea:document.querySelector(".feedback-form textarea")};console.log(e.textarea);
+//# sourceMappingURL=03-feedback.d444d1e2.js.map
