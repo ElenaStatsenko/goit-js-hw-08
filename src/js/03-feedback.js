6e77baba180ed476ea44b1545e3013b6cd557895
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-05-30 22:23:41 +0300

    начала делать 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,37 @@
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('.feedback-form email'),
-    textarea: document.querySelector('.feedback-form textarea'),
-};
-console.log(refs.textarea);
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+//     email: document.querySelector('.feedback-form email'),
+//     textarea: document.querySelector('.feedback-form textarea'),
+// };
+formElement = document.querySelector('.feedback-form'),
+
+
+ formElement.addEventListener('submit', onFormSubmit);
+
+//  refs.textarea.addEventListener ('input', onTextareaInput);
+//  refs.email.addEventListener ('input', onEmailInput);
+ 
+ function onFormSubmit (evt) {
+    evt.preventDefault();
+    const email = evt.currentTarget.email.value;
+    const message = evt.currentTarget.message.value;
+    console.log(message);
+
+
+    
+    // evt.currentTarget.reset();
+    // localStorage.removeItem('feedback-form-state')
+ }
+//  function onTextareaInput (evt) {
+//     const message = evt.currentTarget.value;
+//     localStorage.setItem('feedback-form-state', message);
+//  }
+
+
+
+ function populateTextarea () {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    if (savedMessage) {
+        console.log(savedMessage)
+    }
+ }
