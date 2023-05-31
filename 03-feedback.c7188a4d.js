1b3340f851e7d858ba8728417ec195feb7a9c2ca
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-05-31 14:56:10 +0000

    Deploying to gh-pages from @ ElenaStatsenko/goit-js-hw-08@5a12458c4cb7a7f6ba0321a56ba966dbaf1ea6c7 🚀

--- 03-feedback.c7188a4d.js
+++
@@ -1,2 +0,0 @@
-formElement=document.querySelector(".feedback-form"),formElement.addEventListener("submit",(function(e){e.preventDefault();e.currentTarget.email.value;const t=e.currentTarget.message.value;console.log(t)}));
-//# sourceMappingURL=03-feedback.c7188a4d.js.map
--- 03-feedback.fa6d5f0c.js
+++
@@ -1,2 +0,0 @@
-formElement=document.querySelector(".feedback-form"),formElement.addEventListener("submit",(function(e){e.preventDefault(),e.currentTarget.email.value;var r=e.currentTarget.message.value;console.log(r)}));
-//# sourceMappingURL=03-feedback.fa6d5f0c.js.map
